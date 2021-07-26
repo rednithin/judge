8825fe83eadd6e6b507ffe8504415b3a2f5724d3
rednithin <reddy.nithinpg@live.com>
2021-07-26 18:51:54 +0530

    Don't fail if directory already exists

--- scripts/generate_root_fs_tar.sh
+++ scripts/generate_root_fs_tar.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir tar
+mkdir -p tar
 docker build -f RootFS.Dockerfile -t judge_root_fs .
 CONTAINER_RUN_ID=$(docker run -d judge_root_fs:latest)
 echo "CONTAINER_RUN_ID: $CONTAINER_RUN_ID"
